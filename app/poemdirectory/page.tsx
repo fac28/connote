3cda1f43584ff66dfd3ea0dbc464234da3dc788f
GeorgeKlemperer <george@klemperer.co.uk>
2023-11-22 17:55:19 +0000

    poem cards added

--- app/poemdirectory/page.tsx
+++ app/poemdirectory/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useState, useEffect } from 'react';
+import PoemCard from '../components/PoemCard';
 
 type PoemsType =
   | Array<{
@@ -36,7 +37,7 @@ export default function PoemDirectory () {
             console.log(typeof data[1].display_date)
 
             const filteredPoems = data.filter((poem) => new Date(poem.display_date) <= currentDate);
-            setPoems(filteredPoems);
+            setPoems(data);
         }
       };
       fetchData();
@@ -46,15 +47,17 @@ export default function PoemDirectory () {
     return (
         <>
         <h1>Poems list:</h1>
-        <ul>
-            {poems.map((poem) => (
-                <li key={poem.id}>
-                    <h2>{poem.name} by {poem.author}</h2>
-                    <p>{poem.content}</p>
-                    <p>{poem.display_date}</p>
-                </li>
+        <div className="flex flex-wrap">
+            {poems.map( (poem) => (
+                <PoemCard
+                    key = {poem.id}
+                    poemDate = { poem.display_date}
+                    poemAuthor = { poem.author}
+                    poemName = {poem.name}
+                    poemImage = "https://images.unsplash.com/photo-1575707751065-42256084fbb7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
             ))}
-        </ul>
+        </div>
         </>
     )
 }
