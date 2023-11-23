87483d580a7855b2ce28645b163e5666dab263ee
sgroi-l <laurencesgroi@hotmail.co.uk>
2023-11-23 12:47:02 +0000

    metadata

--- app/poemLibrary/page.tsx
+++ app/poemLibrary/page.tsx
@@ -17,58 +17,63 @@ type PoemsType =
     }>
   | [];
 
-export default function PoemDirectory () {
-    const [poems, setPoems] = useState<PoemsType>([]);  
+export default function PoemDirectory() {
+  const [poems, setPoems] = useState<PoemsType>([]);
 
-    useEffect(() => {
-      const currentDate = new Date();
-      const fetchData = async () => {
-        const supabase = createClientComponentClient();
-        const { data, error } = await supabase.from('poems')
-        .select('id, author, name, content, first_prompt_id, second_prompt_id, third_prompt_id, display_date')
+  useEffect(() => {
+    const currentDate = new Date();
+    const fetchData = async () => {
+      const supabase = createClientComponentClient();
+      const { data, error } = await supabase
+        .from('poems')
+        .select(
+          'id, author, name, content, first_prompt_id, second_prompt_id, third_prompt_id, display_date'
+        )
         .order('display_date', { ascending: false });
 
-        if (error) {
-          console.error('Error fetching data:', error.message);
-        } else {
-            const filteredPoems = data.filter((poem) => new Date(poem.display_date) <= currentDate);
-            setPoems(data); //Change this to filtered poems for production.
-        }
-      };
-      fetchData();
-    }, [setPoems]);
-
-    async function handleSubmit(poemid:number) {
-      const supabase = createClientComponentClient();
-      const { data: sessionData } = await supabase.auth.getSession();
-      if (sessionData?.session?.user?.id) {
-        const userid = sessionData.session.user.id;
-        const responses = await hasUserResponded({userid, poemid}) || []
-        if (responses.length) {
-          window.location.href = `${poemid}/responses`
+      if (error) {
+        console.error('Error fetching data:', error.message);
       } else {
-          window.location.href = `${poemid}/prompts`
+        const filteredPoems = data.filter(
+          (poem) => new Date(poem.display_date) <= currentDate
+        );
+        setPoems(data); //Change this to filtered poems for production.
       }
-    }}
+    };
+    fetchData();
+  }, [setPoems]);
 
-    return (
-        <>
-        <h1>Poems list:</h1>
-        <div className="flex flex-wrap">
-            {poems.map( (poem) => (
-              <span
-                onClick={() => handleSubmit(poem.id)}
-              >
-                <PoemCard
-                    key = {poem.id}
-                    poemDate = { poem.display_date}
-                    poemAuthor = { poem.author}
-                    poemName = {poem.name}
-                    poemImage = "https://images.unsplash.com/photo-1575707751065-42256084fbb7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    />
-              </span>
-            ))}
-        </div>
-        </>
-    )
+  async function handleSubmit(poemid: number) {
+    const supabase = createClientComponentClient();
+    const { data: sessionData } = await supabase.auth.getSession();
+    if (sessionData?.session?.user?.id) {
+      const userid = sessionData.session.user.id;
+      const responses = (await hasUserResponded({ userid, poemid })) || [];
+      if (responses.length) {
+        window.location.href = `${poemid}/responses`;
+      } else {
+        window.location.href = `${poemid}/prompts`;
+      }
+    }
+  }
+
+  return (
+    <>
+      <h1>Poems list:</h1>
+      <div className='flex flex-wrap'>
+        {poems.map((poem) => (
+          // eslint-disable-next-line react/jsx-key
+          <span onClick={() => handleSubmit(poem.id)}>
+            <PoemCard
+              key={poem.id}
+              poemDate={poem.display_date}
+              poemAuthor={poem.author}
+              poemName={poem.name}
+              poemImage='https://images.unsplash.com/photo-1575707751065-42256084fbb7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            />
+          </span>
+        ))}
+      </div>
+    </>
+  );
 }
