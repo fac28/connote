33ee10683886a369d8440870ba4b4fad591798b5
DeepsDali <deepashri.dali@gmail.com>
2023-11-23 11:35:42 +0000

    Toggle Login/Logout button
    Closes #11

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -11,8 +11,9 @@ import {
   Link,
   Button,
 } from "@nextui-org/react";
+import { Session } from "@supabase/auth-helpers-nextjs";
 
-export default function Nav() {
+export default function Nav({ session }: { session: Session | null }) {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = ["Profile", "Poem of the day", "Poem Library"];
@@ -59,17 +60,15 @@ export default function Nav() {
 
       <NavbarContent justify="end">
         <NavbarItem>
-          {/* {session.user ? (
-            <form action="/signout" method="post">
-              <Button className="btn" type="submit" variant="flat">
-                Logout
-              </Button>
-            </form>
-          ) : ( */}
-          <Button as={Link} className="btn" href="/login" variant="flat">
-            Login / Signup
-          </Button>
-          {/* )} */}
+          {session ? (
+            <Button className="btn" type="submit" variant="flat">
+              Logout
+            </Button>
+          ) : (
+            <Button as={Link} className="btn" href="/account" variant="flat">
+              Login / Signup
+            </Button>
+          )}
         </NavbarItem>
       </NavbarContent>
 
