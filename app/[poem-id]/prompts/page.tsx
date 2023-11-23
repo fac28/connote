4bdf794a11ed44413e2a00813334e2ab6c09e667
tess-phillips <tess.phillips@me.com>
2023-11-23 12:26:48 +0000

    dynamic routing for prompts is running!

---
+++ app/[poem-id]/prompts/page.tsx
@@ -0,0 +1,5 @@
+export default function PromptPage() {
+    return (
+        <h1>This is the prompt page!</h1>
+    )
+}
--- app/poemLibrary/page.tsx
+++ app/poemLibrary/page.tsx
@@ -2,6 +2,7 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useState, useEffect } from 'react';
 import PoemCard from '../components/PoemCard';
+import { hasUserResponded } from '@/utils/supabase/models/hasUserResponded';
 
 type PoemsType =
   | Array<{
@@ -18,24 +19,18 @@ type PoemsType =
 
 export default function PoemDirectory () {
     const [poems, setPoems] = useState<PoemsType>([]);  
-  
+
     useEffect(() => {
-        const currentDate = new Date();
-
-
+      const currentDate = new Date();
       const fetchData = async () => {
         const supabase = createClientComponentClient();
         const { data, error } = await supabase.from('poems')
         .select('id, author, name, content, first_prompt_id, second_prompt_id, third_prompt_id, display_date')
-        .order('display_date', { ascending: false })
-;
+        .order('display_date', { ascending: false });
 
         if (error) {
           console.error('Error fetching data:', error.message);
         } else {
-            console.log(data)
-            console.log(typeof data[1].display_date)
-
             const filteredPoems = data.filter((poem) => new Date(poem.display_date) <= currentDate);
             setPoems(data); //Change this to filtered poems for production.
         }
@@ -43,19 +38,35 @@ export default function PoemDirectory () {
       fetchData();
     }, [setPoems]);
 
-      
+    async function handleSubmit(poemid:number) {
+      const supabase = createClientComponentClient();
+      const { data: sessionData } = await supabase.auth.getSession();
+      if (sessionData?.session?.user?.id) {
+        const userid = sessionData.session.user.id;
+        const responses = await hasUserResponded({userid, poemid}) || []
+        if (responses.length) {
+          window.location.href = `${poemid}/responses`
+      } else {
+          window.location.href = `${poemid}/prompts`
+      }
+    }}
+
     return (
         <>
         <h1>Poems list:</h1>
         <div className="flex flex-wrap">
             {poems.map( (poem) => (
+              <span
+                onClick={() => handleSubmit(poem.id)}
+              >
                 <PoemCard
                     key = {poem.id}
                     poemDate = { poem.display_date}
                     poemAuthor = { poem.author}
                     poemName = {poem.name}
                     poemImage = "https://images.unsplash.com/photo-1575707751065-42256084fbb7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                />
+                    />
+              </span>
             ))}
         </div>
         </>
