6bc194244a418ae6ec59a8302f0337b4fcdae8dc
DeepsDali <deepashri.dali@gmail.com>
2023-11-23 12:34:36 +0000

    Fix merge conflict

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,6 @@
-'use client';
-import React from 'react';
+
+"use client";
+import React from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -10,84 +11,84 @@ import {
   NavbarItem,
   Link,
   Button,
-} from '@nextui-org/react';
-import { Session } from '@supabase/auth-helpers-nextjs';
+} from "@nextui-org/react";
+import { Session } from "@supabase/auth-helpers-nextjs";
 
-export default function Nav() {
+export default function Nav({ session }: { session: Session | null }) {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems = ['Profile', 'Poem of the day', 'Poem Library'];
+  const menuItems = ["Profile", "Poem of the day", "Poem Library"];
 
   return (
     <Navbar
-      className={'blue'}
+      className={"blue"}
       isBordered
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
     >
-      <NavbarContent className='sm:hidden pr-3' justify='start'>
+      <NavbarContent className="sm:hidden pr-3" justify="start">
         <NavbarMenuToggle
-          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
         />
       </NavbarContent>
 
-      <NavbarContent className='sm:hidden' justify='center'>
+      <NavbarContent className="sm:hidden" justify="center">
         <NavbarBrand>
-          <p className='logo'>Connote</p>
+          <p className="logo">Connote</p>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarBrand>
-          <p className='logo'>Connote</p>
+          <p className="logo">Connote</p>
         </NavbarBrand>
         <NavbarItem>
-          <Link color='foreground' href='/profile'>
+          <Link color="foreground" href="/profile">
             Profile
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link className='red-400' href='/poemOfTheDay' aria-current='page'>
+          <Link className="red-400" href="/poemOfTheDay" aria-current="page">
             Poem of the day
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className='text-connote_Orange' href='/poemLibrary'>
+          <Link className="text-connote_Orange" href="/poemLibrary">
             Poem Library
           </Link>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify='end'>
+      <NavbarContent justify="end">
         <NavbarItem>
-          {/* {session.user ? (
+          {session ? (
             <form action="/signout" method="post">
-              <Button className="btn" type="submit" variant="flat">
-                Logout
-              </Button>
+        
+              <button  className="btn">Logout</button>
+          
             </form>
-          ) : ( */}
-          <Button as={Link} className='btn' href='/login' variant='flat'>
-            Login / Signup
-          </Button>
-          {/* )} */}
+          ) : (
+            <Button as={Link} className="btn" href="/account" variant="flat">
+              Login / Signup
+            </Button>
+          )}
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarMenu className='mt-1'>
+      <NavbarMenu className="mt-1">
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
-              className='w-full'
+              className="w-full"
               color={
                 index === 2
-                  ? 'warning'
+                  ? "warning"
                   : index === menuItems.length - 1
-                    ? 'danger'
-                    : 'foreground'
+                  ? "danger"
+                  : "foreground"
               }
-              href='#'
-              size='lg'
+              href="#"
+              size="lg"
             >
               {item}
             </Link>
