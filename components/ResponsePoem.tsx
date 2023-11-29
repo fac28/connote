b4105b4f3917e2f8f88be2798a63d08af367d6fc
GeorgeKlemperer <george@klemperer.co.uk>
2023-11-29 16:38:13 +0000

    colours now displayed

--- components/ResponsePoem.tsx
+++ components/ResponsePoem.tsx
@@ -21,40 +21,30 @@ export default function ResponsePoem({
 
   // Function to process and assign highlight_colour
   function processResponses(responses) {
-    // Sort responses by prompt_id
     let sortedResponses = responses.sort((a, b) => a.prompt_id - b.prompt_id);
 
-    // Initialize an object to store the final result
     let result = sortedResponses.reduce((acc, response) => {
-      // If the prompt_id is not in the result object, add it with an empty array
       if (!acc[response.prompt_id]) {
         acc[response.prompt_id] = [];
       }
 
-      // Push the response to the corresponding prompt_id array
       acc[response.prompt_id].push(response);
 
       return acc;
     }, {});
 
-    // Iterate through the result object and assign highlight_colour to the first three responses of each prompt_id
     for (let key in result) {
       result[key].slice(0, 3).forEach((response, index) => {
         response.highlight_colour = topThreeColours[index];
       });
     }
 
-    // Flatten the result object back to an array
     let finalResult = Object.values(result).flat();
 
     return finalResult;
   }
 
-  // Call the function with the provided responses
   let processedResponses = processResponses(responses);
-
-  // Log the result
-  console.log(processedResponses);
 
   return (
     <div className='flex flex-wrap'>
@@ -78,11 +68,22 @@ export default function ResponsePoem({
                           ) && response.prompt_id === selectedPromptNumber
                       );
 
+                      const matchingResponse = processedResponses.find(
+                        (response) =>
+                          response.response_selected.includes(
+                            currentWordIndex
+                          ) && response.prompt_id === selectedPromptNumber
+                      );
+
                       return (
                         <React.Fragment key={currentWordIndex}>
                           <span
                             id={String(currentWordIndex)}
-                            className={isSelected ? 'bg-connote_orange' : ''}
+                            className={
+                              isSelected
+                                ? matchingResponse.highlight_colour
+                                : ''
+                            }
                           >
                             {word}
                           </span>{' '}
