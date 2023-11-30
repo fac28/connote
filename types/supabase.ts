53a195f5f40c9e53f222a7f9a14782f4c2c906d9
sgroi-l <laurencesgroi@hotmail.co.uk>
2023-11-30 16:28:29 +0000

    generated supabase types

--- types/supabase.ts
+++ types/supabase.ts
@@ -7,14 +7,41 @@ export type Json =
   | Json[];
 
 export interface Database {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never;
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+          extensions?: Json;
+        };
+        Returns: Json;
+      };
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
   public: {
     Tables: {
       poems: {
         Row: {
           author: string | null;
           content: string | null;
+          display_date: string | null;
           first_prompt_id: number | null;
           id: number;
+          image: string | null;
           name: string | null;
           second_prompt_id: number | null;
           third_prompt_id: number | null;
@@ -22,8 +49,10 @@ export interface Database {
         Insert: {
           author?: string | null;
           content?: string | null;
+          display_date?: string | null;
           first_prompt_id?: number | null;
-          id?: never;
+          id?: number;
+          image?: string | null;
           name?: string | null;
           second_prompt_id?: number | null;
           third_prompt_id?: number | null;
@@ -31,29 +60,62 @@ export interface Database {
         Update: {
           author?: string | null;
           content?: string | null;
+          display_date?: string | null;
           first_prompt_id?: number | null;
-          id?: never;
+          id?: number;
+          image?: string | null;
           name?: string | null;
           second_prompt_id?: number | null;
           third_prompt_id?: number | null;
         };
-        Relationships: [];
+        Relationships: [
+          {
+            foreignKeyName: 'poems_first_prompt_id_fkey';
+            columns: ['first_prompt_id'];
+            isOneToOne: false;
+            referencedRelation: 'prompts';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'poems_second_prompt_id_fkey';
+            columns: ['second_prompt_id'];
+            isOneToOne: false;
+            referencedRelation: 'prompts';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'poems_third_prompt_id_fkey';
+            columns: ['third_prompt_id'];
+            isOneToOne: false;
+            referencedRelation: 'prompts';
+            referencedColumns: ['id'];
+          },
+        ];
       };
       profiles: {
         Row: {
+          avatar_url: string | null;
+          full_name: string | null;
           id: string;
           updated_at: string | null;
           username: string | null;
+          website: string | null;
         };
         Insert: {
+          avatar_url?: string | null;
+          full_name?: string | null;
           id: string;
           updated_at?: string | null;
           username?: string | null;
+          website?: string | null;
         };
         Update: {
+          avatar_url?: string | null;
+          full_name?: string | null;
           id?: string;
           updated_at?: string | null;
           username?: string | null;
+          website?: string | null;
         };
         Relationships: [
           {
@@ -89,50 +151,129 @@ export interface Database {
       reacts: {
         Row: {
           id: number;
-          reacter_id: number | null;
+          reacter_id: string | null;
           response_id: number | null;
           type: string | null;
         };
         Insert: {
           id?: never;
-          reacter_id?: number | null;
+          reacter_id?: string | null;
           response_id?: number | null;
           type?: string | null;
         };
         Update: {
           id?: never;
-          reacter_id?: number | null;
+          reacter_id?: string | null;
           response_id?: number | null;
           type?: string | null;
         };
-        Relationships: [];
+        Relationships: [
+          {
+            foreignKeyName: 'reacts_reacter_id_fkey';
+            columns: ['reacter_id'];
+            isOneToOne: false;
+            referencedRelation: 'profiles';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'reacts_response_id_fkey';
+            columns: ['response_id'];
+            isOneToOne: false;
+            referencedRelation: 'responses';
+            referencedColumns: ['id'];
+          },
+        ];
       };
       responses: {
         Row: {
           id: number;
+          likes: number | null;
           poem_id: number | null;
           prompt_id: number | null;
-          response_selected: string | null;
+          response_selected: number[] | null;
           response_written: string | null;
-          user_id: number | null;
+          user_id: string | null;
         };
         Insert: {
           id?: never;
+          likes?: number | null;
           poem_id?: number | null;
           prompt_id?: number | null;
-          response_selected?: string | null;
+          response_selected?: number[] | null;
           response_written?: string | null;
-          user_id?: number | null;
+          user_id?: string | null;
         };
         Update: {
           id?: never;
+          likes?: number | null;
           poem_id?: number | null;
           prompt_id?: number | null;
-          response_selected?: string | null;
+          response_selected?: number[] | null;
           response_written?: string | null;
-          user_id?: number | null;
+          user_id?: string | null;
         };
-        Relationships: [];
+        Relationships: [
+          {
+            foreignKeyName: 'responses_poem_id_fkey';
+            columns: ['poem_id'];
+            isOneToOne: false;
+            referencedRelation: 'poems';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'responses_prompt_id_fkey';
+            columns: ['prompt_id'];
+            isOneToOne: false;
+            referencedRelation: 'prompts';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'responses_user_id_fkey';
+            columns: ['user_id'];
+            isOneToOne: false;
+            referencedRelation: 'profiles';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+      user_poem_checks: {
+        Row: {
+          checked: boolean | null;
+          checked_at: string;
+          id: number;
+          poem_id: number | null;
+          user_id: string | null;
+        };
+        Insert: {
+          checked?: boolean | null;
+          checked_at?: string;
+          id?: number;
+          poem_id?: number | null;
+          user_id?: string | null;
+        };
+        Update: {
+          checked?: boolean | null;
+          checked_at?: string;
+          id?: number;
+          poem_id?: number | null;
+          user_id?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'user_poem_checks_poem_id_fkey';
+            columns: ['poem_id'];
+            isOneToOne: false;
+            referencedRelation: 'poems';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'user_poem_checks_user_id_fkey';
+            columns: ['user_id'];
+            isOneToOne: false;
+            referencedRelation: 'profiles';
+            referencedColumns: ['id'];
+          },
+        ];
       };
     };
     Views: {
@@ -148,4 +289,263 @@ export interface Database {
       [_ in never]: never;
     };
   };
+  storage: {
+    Tables: {
+      buckets: {
+        Row: {
+          allowed_mime_types: string[] | null;
+          avif_autodetection: boolean | null;
+          created_at: string | null;
+          file_size_limit: number | null;
+          id: string;
+          name: string;
+          owner: string | null;
+          owner_id: string | null;
+          public: boolean | null;
+          updated_at: string | null;
+        };
+        Insert: {
+          allowed_mime_types?: string[] | null;
+          avif_autodetection?: boolean | null;
+          created_at?: string | null;
+          file_size_limit?: number | null;
+          id: string;
+          name: string;
+          owner?: string | null;
+          owner_id?: string | null;
+          public?: boolean | null;
+          updated_at?: string | null;
+        };
+        Update: {
+          allowed_mime_types?: string[] | null;
+          avif_autodetection?: boolean | null;
+          created_at?: string | null;
+          file_size_limit?: number | null;
+          id?: string;
+          name?: string;
+          owner?: string | null;
+          owner_id?: string | null;
+          public?: boolean | null;
+          updated_at?: string | null;
+        };
+        Relationships: [];
+      };
+      migrations: {
+        Row: {
+          executed_at: string | null;
+          hash: string;
+          id: number;
+          name: string;
+        };
+        Insert: {
+          executed_at?: string | null;
+          hash: string;
+          id: number;
+          name: string;
+        };
+        Update: {
+          executed_at?: string | null;
+          hash?: string;
+          id?: number;
+          name?: string;
+        };
+        Relationships: [];
+      };
+      objects: {
+        Row: {
+          bucket_id: string | null;
+          created_at: string | null;
+          id: string;
+          last_accessed_at: string | null;
+          metadata: Json | null;
+          name: string | null;
+          owner: string | null;
+          owner_id: string | null;
+          path_tokens: string[] | null;
+          updated_at: string | null;
+          version: string | null;
+        };
+        Insert: {
+          bucket_id?: string | null;
+          created_at?: string | null;
+          id?: string;
+          last_accessed_at?: string | null;
+          metadata?: Json | null;
+          name?: string | null;
+          owner?: string | null;
+          owner_id?: string | null;
+          path_tokens?: string[] | null;
+          updated_at?: string | null;
+          version?: string | null;
+        };
+        Update: {
+          bucket_id?: string | null;
+          created_at?: string | null;
+          id?: string;
+          last_accessed_at?: string | null;
+          metadata?: Json | null;
+          name?: string | null;
+          owner?: string | null;
+          owner_id?: string | null;
+          path_tokens?: string[] | null;
+          updated_at?: string | null;
+          version?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'objects_bucketId_fkey';
+            columns: ['bucket_id'];
+            isOneToOne: false;
+            referencedRelation: 'buckets';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      can_insert_object: {
+        Args: {
+          bucketid: string;
+          name: string;
+          owner: string;
+          metadata: Json;
+        };
+        Returns: undefined;
+      };
+      extension: {
+        Args: {
+          name: string;
+        };
+        Returns: string;
+      };
+      filename: {
+        Args: {
+          name: string;
+        };
+        Returns: string;
+      };
+      foldername: {
+        Args: {
+          name: string;
+        };
+        Returns: unknown;
+      };
+      get_size_by_bucket: {
+        Args: Record<PropertyKey, never>;
+        Returns: {
+          size: number;
+          bucket_id: string;
+        }[];
+      };
+      search: {
+        Args: {
+          prefix: string;
+          bucketname: string;
+          limits?: number;
+          levels?: number;
+          offsets?: number;
+          search?: string;
+          sortcolumn?: string;
+          sortorder?: string;
+        };
+        Returns: {
+          name: string;
+          id: string;
+          updated_at: string;
+          created_at: string;
+          last_accessed_at: string;
+          metadata: Json;
+        }[];
+      };
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database['public']['Tables'] & Database['public']['Views'])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database['public']['Tables'] &
+        Database['public']['Views'])
+    ? (Database['public']['Tables'] &
+        Database['public']['Views'])[PublicTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database['public']['Tables']
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database['public']['Tables']
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database['public']['Enums']
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database['public']['Enums']
+    ? Database['public']['Enums'][PublicEnumNameOrOptions]
+    : never;
