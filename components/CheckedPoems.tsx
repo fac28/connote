65bae1199392d01e98a7a8c193a2f8e2b6e3bca2
tess-phillips <tess.phillips@me.com>
2023-11-30 10:28:41 +0000

    added correct images to prfile page

--- components/CheckedPoems.tsx
+++ components/CheckedPoems.tsx
@@ -87,7 +87,7 @@ export default function CheckedPoems() {
               poemDate={poem.display_date}
               poemAuthor={poem.author}
               poemName={poem.name}
-              poemImage='https://images.unsplash.com/photo-1575707751065-42256084fbb7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              poemImage={poem.image}
               poemId={poem.id}
               userId={userId}
               supabase={createClientComponentClient()}
