195162df3dfdaaed2a48acb042a1718928f7a516
sgroi-l <laurencesgroi@hotmail.co.uk>
2023-12-05 16:38:14 +0000

    add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+function NotFoundPage() {
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='my-12 text-4xl'>Page not found</h1>
+
+      <p>Sorry, the page you&apos;re looking for doesn&apos;t exist</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
